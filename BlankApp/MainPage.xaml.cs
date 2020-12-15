d1d97aa7452ae8e33bc977aba136fb0d96f61c69
Ugur Akgul <64159762+ugur-akgul@users.noreply.github.com>
2020-12-15 20:49:14 +0300

    Add project files.

---
+++ BlankApp/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using BlankApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BlankApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<Person> Items { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Items = new ObservableCollection<Person>();
+            Items.Add(new Person { Id = 1, Name = "Ali" });
+            Items.Add(new Person { Id = 2, Name = "Ayşe" });
+            Items.Add(new Person { Id = 3, Name = "Adem" });
+
+            this.BindingContext = this;
+            
+        }
+
+        private  void listView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var selectedPerson = e.SelectedItem as Person;
+            var person = Items.FirstOrDefault(x => x.Id == selectedPerson.Id);
+            person.BgColor = Xamarin.Forms.Color.Green;
+            //await DisplayAlert("ddd","asdsd","asas");
+
+
+        }
+    }
+}
---
+++ BlankApp/Models/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace BlankApp.Models
+{
+    public class Person:INotifyPropertyChanged
+    {
+
+        
+
+        private int id;
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private Xamarin.Forms.Color bgColor = Xamarin.Forms.Color.Blue;
+        public Xamarin.Forms.Color BgColor
+        {
+            get { return bgColor; }
+            set 
+            { 
+                bgColor = value;
+                OnPropertyChanged("BgColor");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged(string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));       
+        
+        }
+
+    }
+}
